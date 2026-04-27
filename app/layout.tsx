c5ab2ef24f5b43e3ac3f37001198727b684c15fe
Bilal Tariq <bilaltariq2k3@gmail.com>
2026-04-27 18:14:19 +0500

    Added review

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
